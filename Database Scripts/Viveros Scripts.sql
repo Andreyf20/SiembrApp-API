cbff0f4a099a9f51856f43ecf9643d5ee9707445
Jose Pablo Feng <joseph.feng1@gmail.com>
2020-12-28 11:04:30 -0600

    Deleted horarios and telefonos table and were merged into Viveros table.

--- Database Scripts/Viveros Scripts.sql
+++ Database Scripts/Viveros Scripts.sql
@@ -1,7 +1,3 @@
-DROP TABLE IF EXISTS Horarios;
-DROP TABLE IF EXISTS Telefonos;
-DROP TABLE IF EXISTS Viveros;
-
 SET timezone = 'America/Costa_Rica';
 
 -- Crear tabla de viveros
@@ -11,52 +7,11 @@ CREATE TABLE Viveros(
 	nombre varchar NOT NULL UNIQUE,
 	direccion varchar NOT NULL,
 	
+	telefonos varchar NOT NULL,
+	horarios varchar NOT NULL,
+	
 	ultimaActualizacion TIMESTAMP NOT NULL,
     borrado boolean NOT NULL
-);
-
--- Crear tabla de horariosXvivero
-
-CREATE TABLE Horarios(
-
-	idHorario BIGSERIAL NOT NULL PRIMARY KEY,
-	
-	-- FK
-	idVivero BIGSERIAL NOT NULL,
-	
-	-- Numero
-	horaInicio varchar NOT NULL,
-	horaFin varchar NOT NULL,
-	-- Dias
-	dias varchar NOT NULL UNIQUE,
-	
-	ultimaActualizacion TIMESTAMP NOT NULL,
-    borrado boolean NOT NULL,
-	
-	FOREIGN KEY (idvivero)
-		REFERENCES Viveros MATCH SIMPLE,
-	
-	UNIQUE(horaInicio,horaFin)
-
-);
-
--- Crear tabla de telefonosXvivero
-CREATE TABLE Telefonos(
-	
-	idTelefono BIGSERIAL NOT NULL PRIMARY KEY,
-	
-	-- FK
-	idVivero BIGSERIAL NOT NULL,
-	
-	-- Numero
-	numeroTel varchar NOT NULL UNIQUE,
-	
-	ultimaActualizacion TIMESTAMP NOT NULL,
-    borrado boolean NOT NULL,
-	
-	FOREIGN KEY (idvivero)
-		REFERENCES Viveros MATCH SIMPLE
-	
 );
 
 -- Funciones
@@ -64,7 +19,9 @@ CREATE TABLE Telefonos(
 -- Agregar
 CREATE OR REPLACE FUNCTION spAgregarVivero(
 	nombreInput varchar,
-	direccionInput varchar
+	direccionInput varchar,
+	telefonosInput varchar,
+	horariosInput varchar
 )
 RETURNS BOOLEAN
 AS
@@ -84,6 +41,8 @@ $$
 				UPDATE Viveros
 					SET
 					direccion = direccionInput,
+					telefonos = telefonosInput,
+					horarios = horariosInput,
 					borrado = FALSE,
 					ultimaActualizacion = NOW() WHERE idViveroLookup = Viveros.idVivero;
 					
@@ -96,8 +55,8 @@ $$
 			
 		ELSE
 		
-			INSERT INTO Viveros (nombre,direccion, ultimaActualizacion,borrado)
-			VALUES (nombreInput,direccionInput, NOW(), FALSE);
+			INSERT INTO Viveros (nombre, direccion, telefonos, horarios, ultimaActualizacion, borrado)
+			VALUES (nombreInput,direccionInput, telefonosInput, horariosInput, NOW(), FALSE);
 				
 			RETURN TRUE;
 		
@@ -107,65 +66,15 @@ $$
 
 $$ LANGUAGE PLPGSQL;
 
-CREATE OR REPLACE FUNCTION spAgregarTelefono(
-	nombreViveroInput varchar,
-	numeroTelefonoInput varchar
-)
-RETURNS BOOLEAN
-AS
-$$
-	DECLARE 
-	
-		idViveroLookup BIGINT := (SELECT V.idVivero FROM Viveros V WHERE LOWER(V.nombre) LIKE LOWER(nombreViveroInput) and V.borrado = FALSE);
-	
-	BEGIN
-		IF idViveroLookup IS NOT NULL THEN
-		
-			INSERT INTO Telefonos(idVivero, numeroTel,ultimaActualizacion,borrado)
-			VALUES(idViveroLookup,numeroTelefonoInput,NOW(),FALSE);
-			
-			RETURN TRUE;
-			
-		ELSE
-			
-			RETURN FALSE;
-			
-		END IF;
-		
-	END;
-$$ LANGUAGE PLPGSQL;
-
-CREATE OR REPLACE FUNCTION spAgregarHorario(
-	nombreViveroInput varchar,
-	horaInicioInput varchar,
-	horaFinInput varchar,
-	diasInput varchar
-)
-RETURNS BOOLEAN
-AS
-$$
-	DECLARE 
-	
-		idViveroLookup BIGINT := (SELECT V.idVivero FROM Viveros V WHERE LOWER(V.nombre) LIKE LOWER(nombreViveroInput) and V.borrado = FALSE);
-	
-	BEGIN
-	
-		IF idViveroLookup IS NOT NULL THEN
-			
-			INSERT INTO Horarios (idVivero,horaInicio, horaFin, dias, ultimaActualizacion, borrado)
-			VALUES (idViveroLookup,horaInicioInput,horaFinInput, diasInput, NOW(), FALSE);
-			
-			RETURN TRUE;
-			
-		ELSE
-			
-			RETURN FALSE;
-			
-		END IF;
-		
-	END;
-$$ LANGUAGE PLPGSQL;
-
+/*
+	select spAgregarVivero('FUNDAZOO',
+	'Calle Ross De la Cruz Roja de Santa Ana 300 metros oeste y 200 metros norte, San Jose, Santa Ana',
+	'2282 8434',
+	'8:00 - 15:30 (Lunes - Viernes)
+	9:00 - 16:30 (Sábados y domingos)')
+	as success;
+	select * from Viveros;
+*/
 
 -- Listar viveros
 
@@ -181,64 +90,41 @@ $$
 	END;
 $$ LANGUAGE PLPGSQL;
 
+/*
+select * from spListViveros();
+*/
 
 -- Consultar informacion de vivero
 
-CREATE OR REPLACE FUNCTION spGetInfoVivero()
-RETURNS TABLE(nombre varchar, direccion varchar)
-AS
-$$
-	DECLARE 
-		
-		idViveroLookup BIGINT := (SELECT V.idVivero FROM Viveros V WHERE LOWER(V.nombre) LIKE LOWER(nombreViveroInput) and V.borrado = FALSE);
-		
-	BEGIN
-	
-		RETURN QUERY
-		SELECT V.nombre, V.direccion FROM Viveros V WHERE V.borrado = FALSE;
-		
-	END;
-$$ LANGUAGE PLPGSQL;
-
-CREATE OR REPLACE FUNCTION spGetHorarioVivero(
+CREATE OR REPLACE FUNCTION spGetInfoVivero(
 	nombreViveroInput varchar
 )
-RETURNS TABLE(vivero varchar, horaInicio varchar, horaFin varchar, dias varchar)
+RETURNS TABLE(nombre varchar, direccion varchar, telefonos varchar, horarios varchar)
 AS
 $$
 	DECLARE 
-	
+		
 		idViveroLookup BIGINT := (SELECT V.idVivero FROM Viveros V WHERE LOWER(V.nombre) LIKE LOWER(nombreViveroInput) and V.borrado = FALSE);
-	
+		
 	BEGIN
 	
 		RETURN QUERY
-		SELECT V.nombre, H.horaInicio, H.horaFin, H.dias FROM Horarios H JOIN Viveros V on V.idVivero = H.idVivero WHERE H.idVivero = idViveroLookup;
+		SELECT V.nombre, V.direccion,V.telefonos,V.horarios FROM Viveros V WHERE V.borrado = FALSE;
 		
 	END;
 $$ LANGUAGE PLPGSQL;
 
-CREATE OR REPLACE FUNCTION spGetTelefonosVivero(
-	nombreViveroInput varchar
-)
-RETURNS TABLE(vivero varchar, telefono varchar)
-AS
-$$
-	DECLARE 
-	
-		idViveroLookup BIGINT := (SELECT V.idVivero FROM Viveros V WHERE LOWER(V.nombre) LIKE LOWER(nombreViveroInput) and V.borrado = FALSE);
-	BEGIN
-	
-		RETURN QUERY
-		SELECT V.nombre,T.numeroTel FROM Telefonos T JOIN Viveros V on V.idVivero = T.idVivero WHERE T.idVivero = idViveroLookup;
-		
-	END;
-$$ LANGUAGE PLPGSQL;
+/*
+select * from spGetInfoVivero('FUNDAZOO');
+*/
 
+-- Modificar datos de vivero
 
-CREATE OR REPLACE FUNCTION spModificarDireccionVivero(
+CREATE OR REPLACE FUNCTION spModificarVivero(
 	nombreViveroInput varchar,
-	direccionNueva varchar
+	direccionNuevaInput varchar,
+	telefonosInput varchar,
+	horariosInput varchar
 ) RETURNS BOOLEAN
 AS
 $$
@@ -251,9 +137,13 @@ $$
 	
 		IF idViveroLookup IS NOT NULL THEN
 			
-			UPDATE Viveros
+			UPDATE Viveros SET
 
-				SET direccion = direccionNueva;
+				direccion = direccionInput,
+				telefonos = telefonosInput,
+				horarios = horariosInput,
+				borrado = FALSE,
+				ultimaActualizacion = NOW() WHERE idViveroLookup = Viveros.idVivero;
 
 			RETURN TRUE;
 		ELSE
@@ -263,14 +153,3 @@ $$
 	END;
 
 $$ LANGUAGE PLPGSQL;
-
-/*
-SELECT spAgregarVivero('FUNDAZOO','Calle Ross De la Cruz Roja de Santa Ana 300 metros oeste y 200 metros norte, San José, Santa Ana') as success;
-SELECT spAgregarTelefono('FUNDAZOO','2282 8434') as success;
-SELECT spAgregarHorario('FUNDAZOO','8:00','15:30','Lunes - Viernes') as success;
-SELECT spAgregarHorario('FUNDAZOO','9:00','16:30','Sábados y domingos') as success;
-
-SELECT * from spGetInfoVivero('FUNDAZOO');
-SELECT * from spGetTelefonosVivero('FUNDAZOO');
-SELECT * from spGetHorarioVivero('FUNDAZOO');
-*/
