3486ded7343e4cda8e7811f190dc626c21321e5d
Andrey Torres <atorresf20@gmail.com>
2021-01-02 01:28:07 -0600

    Fixed bug in spGetUserInfo

--- Database Scripts/Base de datos de usuarios/User DB Stored Procedures.sql
+++ Database Scripts/Base de datos de usuarios/User DB Stored Procedures.sql
@@ -142,12 +142,14 @@ RETURNS BOOLEAN AS $$
 	END;
 $$ LANGUAGE PLPGSQL;
 
--- select spCrearTipoOrganizacion('Asada') as success;
--- select spCrearTipoOrganizacion('Estado') as success;
--- select spCrearTipoOrganizacion('Gobierno local') as success;
--- select spCrearTipoOrganizacion('ONG') as success;
--- select spCrearTipoOrganizacion('Otro') as success;
--- select * from TipoOrganizacion;
+/*
+select spCrearTipoOrganizacion('Asada') as success;
+select spCrearTipoOrganizacion('Estado') as success;
+select spCrearTipoOrganizacion('Gobierno local') as success;
+select spCrearTipoOrganizacion('ONG') as success;
+select spCrearTipoOrganizacion('Otro') as success;
+select * from TipoOrganizacion;
+*/
 
 drop function if exists spUpdateUsuario;
 
@@ -190,4 +192,83 @@ AS $$
     END;
 $$ LANGUAGE PLPGSQL;
 
-select spUpdateUsuario('XD', 'xd3@gmail.com', 'ONG', '860f39d0-6ee6-47a9-a34f-0e449b8bd477') as success;
+-- select spUpdateUsuario('XD', 'xd3@gmail.com', 'ONG', '860f39d0-6ee6-47a9-a34f-0e449b8bd477') as success;
+
+-- Retorna el UUID usando el correo del usuario
+CREATE OR REPLACE FUNCTION spGetUserUUIDwithEmail(
+
+    emailInput varchar
+
+)
+RETURNS VARCHAR
+AS $$		
+	BEGIN
+		
+		RETURN (SELECT U.usuario_uid FROM Users U WHERE U.correo LIKE emailInput);
+		
+    END;
+
+$$ LANGUAGE PLPGSQL;
+
+/*
+SELECT spGetUserUUIDwithEmail('admin@siembrapp.com') as id;
+*/
+
+-- Retorna el ID del usuario usando el UUID
+CREATE OR REPLACE FUNCTION spGetUserIdwithUUID(
+
+    userUUIDInput varchar
+
+)
+RETURNS BIGINT
+AS $$
+	DECLARE
+		
+		inputAsUUID UUID := CAST(userUUIDInput AS UUID);
+		
+	BEGIN
+		
+		RETURN (SELECT U.idusuario FROM Users U WHERE usuario_uid = inputAsUUID);
+		
+    END;
+
+$$ LANGUAGE PLPGSQL;
+
+/*
+SELECT spGetUserIdwithUUID('b66a1bc7-0efb-4417-a20e-9190ab768b32') as "User ID";
+*/
+
+-- Retorna la informacion del usuario por correo
+CREATE OR REPLACE FUNCTION spGetUserInfo(
+
+    correoInput varchar
+
+)
+RETURNS TABLE(
+	uid varchar,
+    nombre varchar,
+	correo varchar,
+	nombretipoOrganizacion varchar,
+	razon varchar,
+	admin boolean)
+AS $$
+	DECLARE
+		
+		nombretipoorganizacion varchar := (SELECT T.nombre FROM users U INNER JOIN tipoOrganizacion T ON U.idTipoOrganizacion = T.idTipoOrganizacion where U.correo = correoInput);
+		uidString varchar :=  (SELECT spGetUserUUIDwithEmail(correoInput));
+		
+	BEGIN
+		
+		RETURN QUERY
+		
+			SELECT uidString as uid, U.nombre as nombre , U.correo as correo, nombretipoorganizacion, U.razon, U.admin as razon
+			FROM public.users U WHERE uidString like U.usuario_uid::varchar;
+		
+    END;
+
+$$ LANGUAGE PLPGSQL;
+
+/*
+select * from spGetUserInfo('admin@siembrapp.com');
+*/
+
